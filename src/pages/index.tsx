c50baa372cb6e8e8616e2ddc94c827ea06d65d6c
timer4ik <timergalievadel@yandex.ru>
2023-04-12 21:55:36 +0300

    +add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,17 @@
-import ProductItem from "@/shared/ui/ProductItem"
-import ProductList from "../entities/ProductList/components/ProductList"
-import { FC } from "react"
-import ProductItemButton from "@/shared/ui/ProductItemButton"
+import type { ReactElement } from 'react'
+import type { NextPageWithLayout } from './_app'
+import { Layout } from '@/shared'
 
-type Props = {
-  children?: React.ReactElement | React.ReactElement[]
+const Page: NextPageWithLayout = () => {
+  return <p>hello world</p>
 }
 
-const Home: FC<Props> = ({
-  children,
-}) => {
-
+Page.getLayout = function getLayout(page: ReactElement) {
   return (
-    <div className="container">
-      <div className="flex-center">
-        <ProductList />
-        <ProductItem
-          title="Электрическая варочная поверхность DEXP 4M2CTYL/B [независимая, конфорок - 2 шт, панель - стеклокерамика, 3.2 кВт]"
-          filledCount={5}
-          availableCount={1}
-          image='/images/product.png'
-          price={25000}
-          actionButtons={
-            <>
-            <ProductItemButton type="cart"></ProductItemButton>
-            <ProductItemButton type="like"></ProductItemButton>
-            </>
-          }
-        ></ProductItem>
-      </div>
-    </div>
+    <Layout>
+        {page}
+    </Layout>
   )
 }
 
-export default Home
+export default Page
